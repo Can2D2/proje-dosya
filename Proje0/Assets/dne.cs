cbc9d90f24cd7596962b7a11d8f7b13bb50de486
Can2D2 <c2d2msi@gmail.com>
2024-07-22 22:26:56 +0300

    oyun3

---
+++ Proje0/Assets/dne.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class dne : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("tak");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void deneme()
+    {
+        Debug.Log("tik");
+    }
+
+}
